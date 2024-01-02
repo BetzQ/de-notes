6dace5f264ad73ae966798bf4e75044a38b36182
muhamad avwan <x9xnobx.19@gmail.com>
2024-01-02 10:51:02 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,15 @@
+window.addEventListener("load", () => {
+  const originalConsoleWarn = console.warn;
+  console.warn = function (message, ...args) {
+    if (
+      message.includes("cdn.tailwindcss.com should not be used in production")
+    ) {
+      return;
+    }
+    originalConsoleWarn.call(this, message, ...args);
+  };
+});
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
